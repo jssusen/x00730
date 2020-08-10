bec8924ab172c9f6dc2ec97f0c2c76c7b28e30c6
13788417687 <5659659+qq360221809@user.noreply.gitee.com>
2020-08-10 18:51:07 +0800

    0810-2

--- application/apis/validate/Withdraw.php
+++ application/apis/validate/Withdraw.php
@@ -9,7 +9,7 @@ class Withdraw extends BaseValidate
 
     protected $rule = [
         "token"     =>"require",
-        "money"     =>"require|isInt",
+        "money"     =>"require",
         "money_position"=>"require",
         "paypwd"        =>"require"
     ];
@@ -18,8 +18,7 @@ class Withdraw extends BaseValidate
     protected $message = [
         "token.require" =>"token不能为空",
         "money.require"=>"提现额度不能为空",
-        "money.isInt"=>"提现额度类型非法",
-        "money_position"=>"提现位置不能为空",
+        "money_position.require"=>"提现位置不能为空",
         "paypwd.require"=>"支付密码不能为空"
     ];
 
